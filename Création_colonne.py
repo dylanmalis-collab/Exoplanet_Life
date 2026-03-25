a7a2fd4a0c21579d9b147b1225812494221a809d
dylanmalis-collab <dylanmalisd@gmail.com>
2026-03-25 14:09:24 +0100

    debut création colonne

--- Création_colonne.py
+++ Création_colonne.py
@@ -1,1 +1,3 @@
-#Création_colonne
+import pandas as pd
+df = pd.read_csv("https://raw.githubusercontent.com/dylanmalis-collab/Exoplanet_Life/refs/heads/main/PSCompPars_2026.03.19_06.16.51.csv")
+print(df.head())
